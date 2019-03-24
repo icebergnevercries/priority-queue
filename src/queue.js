dd47e3ddd863843ab4f7276cfefc2d5abf17a235
icebergnevercries <denislosik@mail.ru>
2019-03-24 23:33:41 +0300

    Implement class Node

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,35 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(this.heap.hsize >= this.maxSize) {
+			throw 'queue has max size';
+		}
+		else {
+			this.heap.push(data, priority);
+		}
 	}
 
 	shift() {
-
+		if(this.heap.hsize <= 0) {
+			throw 'queue is empty';
+		}
+		else {
+			return this.heap.pop();
+		}
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
